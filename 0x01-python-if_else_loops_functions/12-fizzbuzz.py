35b468c115d2d08739e5f3e312e8e13b30f7e860
Lighte22 <107030999+Lighte22@users.noreply.github.com>
2022-09-28 23:07:43 +0100

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 FIZZ = "Fizz"
 BUZZ = "Buzz"
-
-
 def fizzbuzz():
     for number in range(1, 101):
         if (number % 3 and number % 5):
@@ -13,4 +11,5 @@ def fizzbuzz():
             print("%s" % (BUZZ), end=' ')
         else:
             print("%d" % (number), end=' ')
+
          
